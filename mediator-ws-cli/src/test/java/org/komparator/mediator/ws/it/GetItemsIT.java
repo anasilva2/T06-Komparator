cbcc1a108aa1c83db6fd2b16501106c28f2f9433
LeonorClemente <leonor.f.clemente@ist.utl.pt>
2017-04-07 14:01:53 +0100

    GetItemsIT e SearchItemsIT

--- mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/GetItemsIT.java
+++ mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/GetItemsIT.java
@@ -2,8 +2,6 @@ package org.komparator.mediator.ws.it;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import java.util.List;
 
 import org.junit.After;
@@ -12,8 +10,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.komparator.mediator.ws.InvalidItemId_Exception;
-import org.komparator.mediator.ws.InvalidText_Exception;
-import org.komparator.mediator.ws.ItemIdView;
 import org.komparator.mediator.ws.ItemView;
 import org.komparator.supplier.ws.BadProductId_Exception;
 import org.komparator.supplier.ws.BadProduct_Exception;
@@ -31,6 +27,10 @@ public class GetItemsIT extends BaseIT {
 
 	// static members
 	
+	private static SupplierClient s1 = null;
+	private static SupplierClient s2 = null;
+	private static SupplierClient s3 = null;
+	
 	// one-time initialization and clean-up
 	@BeforeClass
 	public static void oneTimeSetUp() throws SupplierClientException, BadProductId_Exception, BadProduct_Exception, InvalidItemId_Exception {
@@ -40,9 +40,9 @@ public class GetItemsIT extends BaseIT {
 		// fill-in test products
 		// (since getProduct is read-only the initialization below
 		// can be done once for all tests in this suite)
-		SupplierClient s1 = new SupplierClient(supplier1URL,supplier1NAME);
-		SupplierClient s2 = new SupplierClient(supplier2URL,supplier2NAME);
-		SupplierClient s3 = new SupplierClient(supplier2URL,supplier3NAME);
+		s1 = new SupplierClient(supplier1URL,supplier1NAME);
+		s2 = new SupplierClient(supplier2URL,supplier2NAME);
+		s3 = new SupplierClient(supplier2URL,supplier3NAME);
 		
 		{
 			ProductView product = new ProductView();
@@ -110,65 +110,89 @@ public class GetItemsIT extends BaseIT {
 	// bad input tests
 
 	@Test(expected = InvalidItemId_Exception.class)
-    public void getItemNullTest() throws InvalidItemId_Exception {
+    public void getItemsNullTest() throws InvalidItemId_Exception {
 		mediatorClient.getItems(null); 
 	}
 	
 	@Test(expected = InvalidItemId_Exception.class)
-	public void getItemEmptyTest() throws InvalidItemId_Exception {
+	public void getItemsEmptyTest() throws InvalidItemId_Exception {
 		mediatorClient.getItems("");
 	}
 	
 	@Test(expected = InvalidItemId_Exception.class)
-	public void getItemWhiteSpaceTest() throws InvalidItemId_Exception {
+	public void getItemsWhiteSpaceTest() throws InvalidItemId_Exception {
 		mediatorClient.getItems(" ");
 	}
 	
 	@Test(expected = InvalidItemId_Exception.class)
-	public void getItemTabTest() throws InvalidItemId_Exception {
+	public void getItemsTabTest() throws InvalidItemId_Exception {
 		mediatorClient.getItems("\t");
 	}
 	
 	@Test(expected = InvalidItemId_Exception.class)
-	public void getProductNewlineTest() throws InvalidItemId_Exception {
+	public void getItemsNewlineTest() throws InvalidItemId_Exception {
 		mediatorClient.getItems("\n");
 	}
 	
 	//main tests
 	
-	@Test
-	public void getItemExistsTest() throws InvalidItemId_Exception {
-		List<ItemView> productList = mediatorClient.getItems("X1");
-		assertNotNull(productList);	
-	}
 	
 	@Test
-	public void getItemAnotherExistsTest() throws InvalidItemId_Exception {
+	public void getItemsExistsTest() throws InvalidItemId_Exception {
 		List<ItemView> productList = mediatorClient.getItems("X1");
 		assertEquals(productList.size(), 3);	
 	}
 	
 	@Test
-	public void getItemAnotherExistsTest2() throws InvalidItemId_Exception {
+	public void getItemsExistsTest2() throws InvalidItemId_Exception {
 		List<ItemView> productList = mediatorClient.getItems("X1");
 		for(ItemView it : productList){
-			String item = it.getDesc();
-				if (item.equals(it)){
-					assertEquals(item, "X1");
-				}
-			
-		}	
+			assertEquals(it.getItemId().getProductId(),"X1");
+		}
+		assertEquals(productList.get(0).getItemId().getSupplierId(),"T06_Supplier2");
+		assertEquals(productList.get(1).getItemId().getSupplierId(),"T06_Supplier1");
+		assertEquals(productList.get(2).getItemId().getSupplierId(),"T06_Supplier3");
 	}
 	
 	@Test
-	public void getItemNotExistsTest() throws InvalidItemId_Exception {
-		// when item does not exist, null should be returned
-		List<ItemView> product = mediatorClient.getItems("Z4");
-		assertEquals(product.size(),0);
+	public void getItemsNotExistsTest() throws InvalidItemId_Exception {
+		List<ItemView> productList = mediatorClient.getItems("Z4");
+		assertEquals(productList.size(),0);
+	}
+
+	@Test
+	public void getItemsSortedByPriceTest() throws InvalidItemId_Exception {
+		List<ItemView> productList = mediatorClient.getItems("X1");
+		assertEquals(productList.get(0).getPrice(),1);
+		assertEquals(productList.get(1).getPrice(),5);
+		assertEquals(productList.get(2).getPrice(),20);
 	}
 	
+	@Test
+	public void getItemsLowercaseNotExistsTest() throws InvalidItemId_Exception {
+		// item identifiers are case sensitive,
+		// so "x1" is not the same as "X1"
+		List<ItemView> productList = mediatorClient.getItems("x1"); 
+		assertEquals(productList.size(),0);
+	}
 	
+	@Test
+	public void getItemsShowItemWithZeroQtyTest() throws InvalidItemId_Exception, BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		s2.buyProduct("X1",1);
+		List<ItemView> productList = mediatorClient.getItems("X1"); 
+		assertEquals(productList.get(0).getItemId().getSupplierId(),"T06_Supplier2");
+		assertEquals(productList.get(0).getPrice(), 1);
+	}
 	
+	@Test
+	public void getItemsWithoutNullInfoTest() throws InvalidItemId_Exception, BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		List<ItemView> productList = mediatorClient.getItems("Z3"); 
+		assertEquals(productList.size(), 1);
+		assertNotNull(productList.get(0).getItemId().getProductId());
+		assertNotNull(productList.get(0).getItemId().getSupplierId());
+		assertNotNull(productList.get(0).getDesc());
+		assertNotNull(productList.get(0).getPrice());
+	}	
 	
 }
 	
---
+++ mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/SearchItemsIT.java
@@ -0,0 +1,219 @@
+package org.komparator.mediator.ws.it;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.komparator.mediator.ws.InvalidItemId_Exception;
+import org.komparator.mediator.ws.InvalidText_Exception;
+import org.komparator.mediator.ws.ItemView;
+import org.komparator.supplier.ws.BadProductId_Exception;
+import org.komparator.supplier.ws.BadProduct_Exception;
+import org.komparator.supplier.ws.BadQuantity_Exception;
+import org.komparator.supplier.ws.InsufficientQuantity_Exception;
+import org.komparator.supplier.ws.ProductView;
+import org.komparator.supplier.ws.cli.SupplierClient;
+import org.komparator.supplier.ws.cli.SupplierClientException;
+
+
+
+/**
+ * Test suite
+ */
+public class SearchItemsIT extends BaseIT {
+
+	// static members
+	
+	private static SupplierClient s1 = null;
+	private static SupplierClient s2 = null;
+	private static SupplierClient s3 = null;
+	
+	// one-time initialization and clean-up
+	@BeforeClass
+	public static void oneTimeSetUp() throws SupplierClientException, InvalidItemId_Exception, BadProductId_Exception, BadProduct_Exception {
+		// clear remote service state before all tests
+		mediatorClient.clear();
+
+		// fill-in test products
+		// (since getProduct is read-only the initialization below
+		// can be done once for all tests in this suite)
+		s1 = new SupplierClient(supplier1URL,supplier1NAME);
+		s2 = new SupplierClient(supplier2URL,supplier2NAME);
+		s3 = new SupplierClient(supplier2URL,supplier3NAME);
+		
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(5);
+			product.setQuantity(10);
+			s1.createProduct(product);
+		}	
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(7);
+			product.setQuantity(1);
+			s2.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("T3");
+			product.setDesc("Basketball");
+			product.setPrice(20);
+			product.setQuantity(10);
+			s3.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y2");
+			product.setDesc("Baseball");
+			product.setPrice(20);
+			product.setQuantity(20);
+			s1.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Z3");
+			product.setDesc("Soccer Ball");
+			product.setPrice(30);
+			product.setQuantity(30);
+			s2.createProduct(product);
+		}
+		 
+	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+		// clear remote service state after all tests
+		mediatorClient.clear();
+	}
+
+	// members
+
+	// initialization and clean-up for each test
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	// tests
+	// assertEquals(expected, actual);
+
+	// bad input tests
+
+	@Test(expected = InvalidText_Exception.class)
+    public void searchItemNullTest() throws InvalidText_Exception {
+		mediatorClient.searchItems(null);
+	}
+	
+	@Test(expected = InvalidText_Exception.class)
+    public void searchItemEmptyItemTest() throws InvalidText_Exception {
+		mediatorClient.searchItems("");
+	}
+	
+	@Test(expected = InvalidText_Exception.class)
+    public void searchItemWhiteSpaceTest() throws InvalidText_Exception {
+		mediatorClient.searchItems(" ");
+	}
+	
+	@Test(expected = InvalidText_Exception.class)
+    public void searchItemTabTest() throws InvalidText_Exception {
+		mediatorClient.searchItems("\t");
+	}
+	
+	@Test(expected = InvalidText_Exception.class)
+    public void searchItemNewLineTest() throws InvalidText_Exception {
+		mediatorClient.searchItems("\n");
+	}
+	
+	
+	// main tests
+	@Test
+	public void searchItemExistsTest() throws InvalidText_Exception {
+		List<ItemView> productList = mediatorClient.searchItems("Baseball");
+		assertEquals(1, productList.size());
+		assertEquals(productList.get(0).getDesc().contains("Baseball"), true);	
+	}
+	
+	@Test
+	public void searchItemAnotherExistsTest() throws InvalidText_Exception {
+		List<ItemView> productList = mediatorClient.searchItems("Basketball");
+		assertEquals(3, productList.size());
+		for(ItemView i: productList){
+			assertEquals(i.getDesc().contains("Basketball"), true);
+		}
+		assertEquals(productList.get(0).getItemId().getSupplierId(), "T06_Supplier3");
+		assertEquals(productList.get(1).getItemId().getSupplierId(), "T06_Supplier1");
+		assertEquals(productList.get(2).getItemId().getSupplierId(), "T06_Supplier2");
+	}
+	
+	@Test
+	public void searchItemAnotherExistsTest2() throws InvalidText_Exception{
+		List<ItemView> productList = mediatorClient.searchItems("ball");
+		assertEquals(4, productList.size());
+		for(ItemView prod : productList){
+			assertEquals(prod.getDesc().contains("ball"),true);
+		}
+	}
+	
+	@Test
+	public void searchItemDoesNotExistsTest1() throws InvalidText_Exception{
+		List<ItemView> productList = mediatorClient.searchItems("Soccerball");
+		assertEquals(0, productList.size());
+	}
+	
+	@Test
+	public void searchItemCaseSensitiveTest() throws InvalidText_Exception{
+		List<ItemView> productList = mediatorClient.searchItems("Ball");
+		assertEquals(1, productList.size());
+		assertEquals(productList.get(0).getDesc().contains("Ball"), true);
+	}
+	
+	@Test
+	public void searchItemWithZeroQtyTest() throws InvalidText_Exception, BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception{
+		s2.buyProduct("X1",1);
+		List<ItemView> productList = mediatorClient.searchItems("Basketball");
+		assertEquals(3, productList.size());
+		for(ItemView i: productList){
+			assertEquals(i.getDesc().contains("Basketball"), true);
+		}
+		
+	}
+	
+	@Test
+	public void searchItemSortedByIdandPriceTest() throws InvalidText_Exception, BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception{
+		List<ItemView> productList = mediatorClient.searchItems("Basketball");
+		assertEquals(3, productList.size());
+		
+		assertEquals(productList.get(0).getItemId().getProductId(), "T3");
+		assertEquals(productList.get(1).getItemId().getProductId(), "X1");
+		assertEquals(productList.get(1).getPrice(), 5);
+		assertEquals(productList.get(2).getItemId().getProductId(), "X1");
+		assertEquals(productList.get(2).getPrice(), 7);
+	}
+	
+	@Test
+	public void searchItemWithoutNullInfoTest() throws InvalidText_Exception, BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception{
+		List<ItemView> productList = mediatorClient.searchItems("Baseball");
+		assertEquals(1, productList.size());
+		
+		assertNotNull(productList.get(0).getItemId().getProductId());
+		assertNotNull(productList.get(0).getItemId().getSupplierId());
+		assertNotNull(productList.get(0).getDesc());
+		assertNotNull(productList.get(0).getPrice());
+	}
+	
+
+}
